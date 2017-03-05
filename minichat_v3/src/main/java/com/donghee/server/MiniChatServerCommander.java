d3707fb3255ab017cdf7d631e4649c23523d42be
terdong <terdong@gmail.com>
2017-03-05 09:36:24 +0900

    /help 명령어 추가

--- minichat_v3/src/main/java/com/donghee/server/MiniChatServerCommander.java
+++ minichat_v3/src/main/java/com/donghee/server/MiniChatServerCommander.java
@@ -26,10 +26,18 @@ public class MiniChatServerCommander implements Runnable {
     @Override
     public void run() {
 
+        System.out.println("도움말을 보시려면 /help를 입력하세요.");
+
         for(; isAlive;) {
             String command = scanner.nextLine();
 
-            if(command.equals("/allUser")){
+            if(command.equals("/help")){
+                System.out.println("명령어 리스트");
+                System.out.println("/allUser: 접속한 모든 유저 수");
+                System.out.println("/exit: 서버종료");
+            }
+            else if(command.equals("/allUser")){
+
                 commander.displayCurrentUserCount();
             }else if(command.equals("/exit")){
                 System.out.println("서버를 종료하겠습니다.");
---
+++ minichat_v3/src/main/java/com/donghee/test/SwingTest.java
@@ -0,0 +1,15 @@
+package com.donghee.test;
+
+import javax.swing.JButton;
+
+/**
+ * Created by Administrator on 2017-03-04.
+ */
+
+public class SwingTest {
+    public static void main(String[] args) {
+
+        JButton jb = new JButton();
+
+    }
+}
